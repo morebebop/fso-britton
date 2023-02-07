79f02115cbad75f1d9a80fc9748c8f2aa3fc1f45
morebebop <brittonjwright@gmail.com>
2023-02-07 10:04:18 -0500

    Refactor code to update parts objects  (ex: 1.3)

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,56 +1,33 @@
 const App = () => {
-  // const-definitions
-  const Header = (props) => {
-    return (
-      <div>
-        <h1>
-          {props.course}
-        </h1>
-      </div>
-    )
-  }
-  const Part = (props) => {
-    return (
-      <p>
-        {props.part} {props.exercise}
-      </p>
-    )
-  }
-
-  const Content = (props) => {
-    return (
-      <div>
-        <Part part={props.part1} exercise={exercises1}/>
-        <Part part={props.part2} exercise={exercises2}/>
-        <Part part={props.part3} exercise={exercises3}/>
-      </div>
-    )
-  }
-
-  const Total = (props) => {
-    return (
-      <div>
-        <p>
-          Number of exercises {props.exercises1 + props.exercises2 +props.exercises3}
-        </p>
-      </div>
-    )
-  }
-
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3}
-      exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <h1>{course}</h1>
+      <p>
+        {part1.name} {part1.exercises}
+      </p>
+      <p>
+        {part2.name} {part2.exercises}
+      </p>
+      <p>
+        {part3.name} {part3.exercises}
+      </p>
+      <p>
+        Number of exercises {part1.exercises + part2.exercises + part3.exercises} 
+      </p>
     </div>
   )
 }
