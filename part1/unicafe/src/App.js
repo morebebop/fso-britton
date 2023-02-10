5a688c16b6f8d6c13a0194661d653b23ebd19763
morebebop <brittonjwright@gmail.com>
2023-02-10 16:47:09 -0500

    Feat: add average and positive percent
    This commit adds average and positive percent values to the
    Statistics section. The formulas for both are calculated in the object
    in each p element. This is a temporary fix and will be refactored during
    the upcoming exercises

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,11 +11,14 @@ const App = () => {
       <h1>give feedback</h1>
         <button onClick={() => setGood(good + 1)}>good</button>
         <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>good</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {good + neutral + bad}</p>
+        <p>average {((good * 1) + (neutral * 0) +( bad * -1))/(good + neutral + bad)}</p>
+        <p>positive {(good / (good + neutral + bad))*100}%</p>
     </div>
   )
 }
