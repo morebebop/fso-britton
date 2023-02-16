578c07200ecb818c5248ac074e02fc626b990e6a
morebebop <brittonjwright@gmail.com>
2023-02-16 09:53:10 -0500

    Refactor: update code to access nested courses
    Exercise 2.4. This commit adds an additional course to the courses object. The Course,
    Header, Content, and Part components have been updated to access the
    course object by destructuring it. Additionally, each components uses
    the map function to loop through and display each course.

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -1,58 +1,105 @@
-const Header = ({course}) => {
+// the Header component displays the name for each course in an h2 
+const Header = ({courseName}) => {
   return (
-    <div>
-      <h1>{course}</h1>
-    </div>
+    <h2 key={courseName.id}>{courseName}</h2>
   )
 }
 
-// the Content component uses the map function to create a return a p element with 
-// name and exercise for each course part
-const Content =({parts}) => {
+// the Part component uses the map function to loop through each index in the parts array to display the part name and exercise in a p element
+const Part = ({parts}) => {
+  return (
+    parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)
+  )
+}
+
+// the Total component uses the reduce function to loop through each part's exercise count and add them to a sum value (starting at 0)
+// this value is assigned to the Total const and displayed in a p element
+const Total = ({parts}) => {
   const total = parts.reduce((sum, part) => sum + part.exercises, 0)
   return (
-    <div>
-      {parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
-      <p><b>total of {total} exercises</b></p>
-    </div>
+    <p><b>total of {total} exercises</b></p>
   )
-
 }
 
-const Course = ({course}) => {
+// the Content component passes the parts array to both Part and Total components to display each part and its total sum in a single div
+const Content =({parts}) => {
   return (
     <div>
-        <Header course={course.name} />
-        <Content parts={course.parts}/>
+      <Part parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+  
+}
+
+// the Course component uses the destructured courses object to pass it along to the Header and Content component directly. 
+const Course = ({courses}) => {
+  return (
+    <div>
+      {/* map is used to loop over each course in the courses object. It displays te course name and its parts in a separate div */}
+      {courses.map(course =>
+        <div key={course.id}>
+          {/* the Header component accesses the name for each course */}
+          <Header courseName={course.name} />
+          {/* the Content component accesses the part array in each course */}
+          <Content parts={course.parts}/>
+        </div>
+      )}
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
   return (
-    <Course course={course} />
+    <div>
+      <h1>Web development curriculum</h1>
+      {/* the Course component is called with the courses object as an argument */}
+      <Course courses={courses} />
+    </div>
   )
 }
 
