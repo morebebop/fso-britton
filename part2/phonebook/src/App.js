bf76349f12d1ccb780c0bfca22688657628ead21
morebebop <brittonjwright@gmail.com>
2023-02-19 13:22:58 -0500

    Feat: Adds the name submission and display to page
    This commit adds a form to submit new names for the phonebook. Also, it
    adds a section below the Numbers h2 to display each added Name.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,50 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  // calling two state consts. persons will handle the list of people in the phone book]
+  // name will handle the names entered in the phonebook
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
+
+  // addName called on form submission
+  const addName = (event) => {
+    // prevents the form from refreshing the page after submission
+    event.preventDefault()
+    // call setPersons to create a new array with a copy of the old persons array and the newName
+    // entered in the phonebook as the last object in the array.
+    setPersons(
+      [
+        ...persons,
+        {key: newName, name: newName}
+      ]
+    )
+    // returns the input field to empty after form submission
+    setNewName('')
+  }
+
+  // sets the newName to whatever is entered in the input field
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: 
+          <input 
+          value={newName}
+          onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+        {/* takes the persons array and displays it as a p on page */}
+        {persons.map(person => <p key={person.name}>{person.name}</p>)}
     </div>
   )
 }
