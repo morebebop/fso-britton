60f77d54127fa76fa51c73fed7eb95fd2c694dbb
morebebop <brittonjwright@gmail.com>
2023-02-21 17:26:58 -0500

    Feat: add filter field
    Exercise 2.9. This commit adds a filter input field that returns a
    filtered search results array in the numbers sections. When the search
    input is empty, the full persons array is displayed. Otherwise the
    filtered search result list will be returned.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,28 @@
 import { useState } from 'react'
 
 const App = () => {
-  // calling three state consts. persons will handle the list of people in the phone book]
-  // name will handle the names entered in the phonebook. number handles phone numbers
-  const [persons, setPersons] = useState([]) 
+  // persons state hook holds the array for a persons name and number
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  // newName state hook sets the name for a person in the persons array
   const [newName, setNewName] = useState('')
+  // newNumber state hook sets the number for a person in the persons array
   const [newNumber, setNewNumber] = useState([])
-
+  // search state hook is defined with a value for what is searched for and a results array for the search results
+  const [search, setSearch] = useState({
+    search: '',
+    results: []
+  })
+  
   // addName called on form submission
   const addName = (event) => {
     // prevents the form from refreshing the page after submission
     event.preventDefault()
-
+    
     // checks if name entered into field is already in phonebook object. DOES NOT check for
     // alternative capitalization. 'Harry Potter' !== 'harry potter' (true)
     if (persons.some(name => name.name === newName)) {
@@ -19,34 +30,59 @@ const App = () => {
       setNewNumber('')
       return (
         alert(`${newName} is already added to phonebook`)
-      )
+        )
+      }
+      // call setPersons to create a new array with a copy of the old persons array and the name/number
+      // entered in the phonebook as the last object in the array.
+      setPersons(
+        [
+          ...persons,
+          {key: newName, name: newName, number: newNumber}
+        ]
+        )
+        // returns the input fields to empty after form submission
+        setNewName('')
+        setNewNumber('')
+      }
+      
+      // event handler for name field. updates newName to whatever has been typed
+      const handleNameChange = (event) => {
+        setNewName(event.target.value)
     }
-    // call setPersons to create a new array with a copy of the old persons array and the name/number
-    // entered in the phonebook as the last object in the array.
-    setPersons(
-      [
-        ...persons,
-        {key: newName, name: newName, number: newNumber}
-      ]
-    )
-    // returns the input fields to empty after form submission
-    setNewName('')
-    setNewNumber('')
-  }
 
- // event handler for name field. updates newName to whatever has been typed
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    // event handler for number field. updates newNumber to whatever has been typed
+    const handleNumberChange = (event) => {
+      setNewNumber(event.target.value)
+    }
 
-  // event handler for number field. updates newNumber to whatever has been typed
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  return (
-    <div>
+    // event handler for the search field. 
+    const handleSearchChange = (event) => {
+      // updates the search field to whatever has been typed.
+      setSearch({search: event.target.value})
+      // results is assigned the value of the persons array filtered for any person that contains values in the search field.
+      // this searches only for the name field and not the number field.
+      const results = persons.filter(person => {
+        // checks for a value for of the search and returns the persons array if search input is empty.
+        if (event.target.value === "") {
+          return (
+            person
+          )
+        }
+        return (
+          person.name.toLowerCase().includes(event.target.value.toLowerCase())
+        )
+      })
+      setSearch ({
+        search: event.target.value,
+        list: results
+      })
+    }
+    
+    return (
+      <div>
       <h2>Phonebook</h2>
+      filter shown with <input value={search.search} onChange={handleSearchChange} type='search'/>
+      <h2>add a New</h2>
       <form onSubmit={addName}>
         <div>
           name: 
@@ -60,8 +96,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {/* takes the persons array and displays it as a p on page */}
-        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {/* if the search field is empty, then the persons array is display. Otherwise, the search results are displayed */}
+      {search.search === '' ? persons.map(person => <p key={person.name}>{person.name} {person.number}</p>) 
+                            : search.list.map(s => <p key={s.name}>{s.name} {s.number}</p>)}
     </div>
   )
 }
