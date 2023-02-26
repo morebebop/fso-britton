93b44dc7db2baae2f08fd87babccc1f5af9e8451
morebebop <brittonjwright@gmail.com>
2023-02-26 09:18:41 -0500

    Add db.json and axios to add promises
    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Person from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
   // persons state hook holds the array for a persons name and number
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   // newName state hook sets the name for a person in the persons array
   const [newName, setNewName] = useState('')
   // newNumber state hook sets the number for a person in the persons array
@@ -20,6 +16,16 @@ const App = () => {
     search: '',
     results: []
   })
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
   
   // addName called on form submission
   const addName = (event) => {
