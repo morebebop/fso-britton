bba5b27f05f8988cd5d47b2415802d0579592321
morebebop <brittonjwright@gmail.com>
2023-03-22 11:45:31 -0400

    Fix: remove bug from stopping post route

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -13,13 +13,20 @@ blogRouter.get('/api/blogs', (request, response) => {
 })
   
 // CREATE
-blogRouter.post('/api/blogs', (request, response) => {
-const blog = new Blog(request.body)
-    Blog
-        .save()
-        .then(result => {
-        response.status(201).json(result)
+blogRouter.post('/api/blogs', (request, response, next) => {
+    const body = request.body
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    })
+    blog.save()
+        .then(savedBlog => {
+        response.json(savedBlog)
         })
+        .catch(error => next(error))
 })
 
 module.exports = blogRouter
